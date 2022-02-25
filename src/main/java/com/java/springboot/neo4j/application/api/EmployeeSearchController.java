bc1759c3ca77c472bffbee5fb0918c6d130ed6ba
vinod Singh <vinod.amity@gmail.com>
2022-02-25 15:03:34 +0530

    Implemenation of CRUD operations on Employee Object in Org Hierarchy

--- src/main/java/com/java/springboot/neo4j/application/api/EmployeeSearchController.java
+++ src/main/java/com/java/springboot/neo4j/application/api/EmployeeSearchController.java
@@ -1,10 +1,17 @@
 package com.java.springboot.neo4j.application.api;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,38 +29,84 @@ public class EmployeeSearchController {
 	@Autowired
 	private EmployeeSearchService employeeSearchService;
 
-	@ApiOperation(value = "Search Employee by employee' name")
+	@ApiOperation(value = "Search employee by employee's name")
 	@GetMapping("/searchby/name/{name}")
 	public List<Employee> findByName(@PathVariable("name") String name) {
 		return employeeSearchService.getEmployeeByName(name);
 	}
 
-	@ApiOperation(value = "List All Employees")
+	@ApiOperation(value = "List all employees")
 	@GetMapping("/list")
 	public List<Employee> findAllEmployee() {
 		return employeeSearchService.getAll();
 	}
 
-	/*
-	 * @GetMapping("/employee/searchby/empid/{empid}") public Employee
-	 * findByEmpId(@PathVariable("empid") String empid) { return
-	 * employeeSearchService.fetchEmployeeByEmpId(empid); }
-	 * 
-	 * @GetMapping("/movie/{title}") public MovieDetailsDto
-	 * findByTitle(@PathVariable("title") String title) { return
-	 * employeeSearchService.fetchDetailsByTitle(title); }
-	 * 
-	 * @GetMapping("/movie/{title}") public MovieDetailsDto
-	 * findByTitle(@PathVariable("title") String title) { return
-	 * employeeSearchService.fetchDetailsByTitle(title); }
-	 * 
-	 * @GetMapping("/movie/{title}") public MovieDetailsDto
-	 * findByTitle(@PathVariable("title") String title) { return
-	 * employeeSearchService.fetchDetailsByTitle(title); }
-	 * 
-	 * @GetMapping("/movie/{title}") public MovieDetailsDto
-	 * findByTitle(@PathVariable("title") String title) { return
-	 * employeeSearchService.fetchDetailsByTitle(title); }
-	 */
+	@ApiOperation(value = "Get employee details by empId")
+	@GetMapping("/searchby/empId/{empId}")
+	public Employee findByEmpId(@PathVariable("empId") Long empId) {
+		return employeeSearchService.findByEmpId(empId);
+	}
+
+	@ApiOperation(value = "List all employees by city")
+	@GetMapping("/searchby/city/{city}")
+	public List<Employee> findByCity(@PathVariable("city") String city) {
+		return employeeSearchService.findByCity(city);
+	}
+
+	@ApiOperation(value = "List employees based on gender")
+	@GetMapping("/searchby/gender/{gender}")
+	public List<Employee> findByGender(@PathVariable("gender") String gender) {
+		return employeeSearchService.findByGender(gender);
+	}
+
+	@ApiOperation(value = "Add a new employee and relationship to an existing employee")
+	@PutMapping("/add/new")
+	public String addEmployee(@RequestBody Employee employee, Long toEmpId) {
+		return employeeSearchService.addEmployee(employee, toEmpId);
+	}
+
+	@ApiOperation(value = "Add relationship for an existing employee to existing supervisor")
+	@PostMapping("/add/relationship/for/reportees/to/supervisor")
+	public String addRelationshipTo(@RequestBody Map<Long, String> reportees, Long SupervisorEmpId) {
+		return addRelationshipToSupervisor(reportees.keySet(), SupervisorEmpId);
+	}
+
+	@ApiOperation(value = "Delete an employee and its associated relationship from heirarchy")
+	@PostMapping("/delete/employee")
+	public String deleteEmployee(@RequestBody Map<Long, String> reportees) {
+		return deleteEmployee(reportees.keySet());
+	}
+
+	private String addRelationshipToSupervisor(Set<Long> reporteesEmpId, Long supervisorEmpId) {
+		boolean flag = Boolean.FALSE;
+		String reponseStr = StringUtils.EMPTY;
+		Iterator<Long> itr = reporteesEmpId.iterator();
+		int i = 0;
+		while (itr.hasNext()) {
+			reponseStr = employeeSearchService.addRelationshipTo(itr.next(), supervisorEmpId);
+			i++;
+			if (i == reporteesEmpId.size())
+				flag = Boolean.TRUE;
+		}
+		return flag
+				? String.format(
+						"Relationship- \"%s\" has been created for reportees empId's : %s to supervisor's empId: %s",
+						reponseStr, StringUtils.join(reporteesEmpId.toString(), ", "), supervisorEmpId.toString())
+				: "Request not processed.";
+	}
+
+	private String deleteEmployee(Set<Long> reporteesEmpId) {
+		boolean flag = Boolean.FALSE;
+		Iterator<Long> itr = reporteesEmpId.iterator();
+		int i = 0;
+		while (itr.hasNext()) {
+			employeeSearchService.deleteEmployee(itr.next());
+			i++;
+			if (i == reporteesEmpId.size())
+				flag = Boolean.TRUE;
+		}
+		return flag ? String.format("Employee(s)- \"%s\" has been deleted from heirarchy",
+				StringUtils.join(reporteesEmpId.toString(), ", ")) : "Request not processed.";
+	}
 
 }
--- src/main/java/com/java/springboot/neo4j/application/service/EmployeeSearchService.java
+++ src/main/java/com/java/springboot/neo4j/application/service/EmployeeSearchService.java
@@ -4,22 +4,52 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.java.springboot.neo4j.application.data.Employee;
 import com.java.springboot.neo4j.application.repository.EmployeeRepository;
 
 @Service
 public class EmployeeSearchService {
-	
+
 	@Autowired
 	private EmployeeRepository employeeRepository;
-	
+
 	public List<Employee> getEmployeeByName(String name) {
-		return employeeRepository.findByName(name);
+		return employeeRepository.findByNameContainsIgnoreCase(name);
 	}
 
 	public List<Employee> getAll() {
 		return employeeRepository.findAll();
+	}
+
+	public List<Employee> findByGender(String gender) {
+		return employeeRepository.findByGenderIgnoreCase(gender);
+	}
+
+	public List<Employee> findByCity(String city) {
+		return employeeRepository.findByCityContainsIgnoreCase(city);
+	}
+
+	public Employee findByEmpId(Long empId) {
+		return employeeRepository.findByEmpId(empId);
+	}
+
+	@Transactional
+	public String addEmployee(Employee employee, Long toEmpId) {
+		employee = employeeRepository.save(employee);
+		String result = addRelationshipTo(employee.getEmpId(), toEmpId);
+		result = String.format("Relationship- \"%s\" created for reportee's empId:%s to supervisor's empId:%s", result, employee.getEmpId().toString(),
+				toEmpId.toString());
+		return result;
+	}
+
+	public String addRelationshipTo(Long reporteeEmpId, Long toEmpId) {
+		return employeeRepository.createRelationship(reporteeEmpId, toEmpId);
+	}
+
+	public void deleteEmployee(Long empId) {
+		employeeRepository.deleteEmployeeWithAllRelationship(empId);
 	}
 
 }
